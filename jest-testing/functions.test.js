3c29a7d0d9c8cee72cbdcc318eebae193d07162b
Zachary Wright <80498861+wrightzachary@users.noreply.github.com>
2022-02-03 23:08:05 -0600

    adding async test

--- jest-testing/functions.test.js
+++ jest-testing/functions.test.js
@@ -47,9 +47,18 @@ test('Admin should be in  usernames', () =>{
 });
 
 // working with async data
+
+// Promise
 test('User fetched name should be Leanne Graham', () =>{
     expect.assertions(1);
     return functions.fetchUser().then(data => {
         expect(data.name).toEqual('Leanne Graham');
     })
-})
+});
+
+// Async Await
+test('User fetched name should be Leanne Graham', async () =>{
+    expect.assertions(1);
+    const data = await functions.fetchUser();
+    expect(data.name).toEqual('Leanne Graham');
+});
